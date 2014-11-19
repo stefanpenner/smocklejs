bd52e443c8208d3ecbda8f008b7fd6e84fea5448
Clay Miller <clay@smockle.com>
2014-11-19 08:29:13 -0600

    Port some of the rails site.

--- app/router.js
+++ app/router.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import App from './app';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
@@ -6,6 +7,27 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  return this.route('index', {
+    path: '/'
+  });
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    this.store.push('project', {
+      name: "Smockle",
+      client: "client",
+      quote: "quote",
+      description: "description",
+      link: "http://www.smockle.com/"
+    });
+  }
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('project');
+  }
 });
 
 export default Router;
